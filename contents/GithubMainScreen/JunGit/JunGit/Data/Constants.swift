49330872a2fd6b9ea1fb80732517daa1dc444edd
이창준 <crystalashe@naver.com>
2022-10-24 12:30:06 +0900

    [Feat] 상수 파일 분리

---
+++ contents/GithubMainScreen/JunGit/JunGit/Data/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  JunGit
+//
+//  Created by 이창준 on 2022/10/24.
+//
+
+import Foundation
+
+struct K {
+    
+    // MARK: - Commons
+    static let userName: String = "StanSign"
+    
+    // MARK: - Identifiers
+    
+    // MARK: - Icons
+    struct Icon {
+        static let shareIcon = "square.and.arrow.up"
+        static let settingsIcon = "gearshape"
+    }
+}
