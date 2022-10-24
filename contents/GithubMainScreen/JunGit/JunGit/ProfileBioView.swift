11345742c84650fdb5eacfdce9d6a8683d2312a4
이창준 <crystalashe@naver.com>
2022-10-24 15:21:54 +0900

    [Feat] 바이오뷰 Designable 설정, 뷰 클래스 설정

--- contents/GithubMainScreen/JunGit/JunGit/ProfileBioView.swift
+++ contents/GithubMainScreen/JunGit/JunGit/ProfileBioView.swift
@@ -7,14 +7,7 @@
 
 import UIKit
 
-class ProfileBioView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+@IBDesignable
+final class ProfileBioView: UIView {
 
 }
