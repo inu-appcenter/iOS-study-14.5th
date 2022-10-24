883a8409a415751179b5a5ff131892b44580d3b8
이창준 <crystalashe@naver.com>
2022-10-24 14:05:42 +0900

    [Feat] 네비게이션 뷰 고정 형태 구현

--- contents/GithubMainScreen/JunGit/JunGit/ViewController.swift
+++ contents/GithubMainScreen/JunGit/JunGit/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
+    // Scroll View
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // Navigation Bar
+    @IBOutlet weak var profileLabel: UILabel!
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.configureUI()
     }
 
-
+    // MARK: - IBActions
+    @IBAction func settingButtonTapped(_ sender: UIButton) {
+        print("Setting Button Tapped")
+    }
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        print("Share Button Tapped")
+    }
 }
 
+// MARK: - UI
+private extension ViewController {
+    func configureUI() {
+        self.profileLabel.text = K.userName
+        self.settingButton.setImage(
+            UIImage(systemName: K.Icon.settingsIcon),
+            for: .normal)
+        self.shareButton.setImage(
+            UIImage(systemName: K.Icon.shareIcon),
+            for: .normal)
+    }
+}
