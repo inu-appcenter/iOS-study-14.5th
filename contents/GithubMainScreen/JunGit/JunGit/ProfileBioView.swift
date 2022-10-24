61d8220f57ff182235662aa5b33ea89a19504c0f
이창준 <crystalashe@naver.com>
2022-10-24 15:18:09 +0900

    [Feat] 프로필 설명을 위한 커스텀뷰 생성

---
+++ contents/GithubMainScreen/JunGit/JunGit/ProfileBioView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileBioView.swift
+//  JunGit
+//
+//  Created by 이창준 on 2022/10/24.
+//
+
+import UIKit
+
+class ProfileBioView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
