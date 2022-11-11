fa7c16203e5c06067eae0b2e28e692295a80e66f
eung7 <wannabeeung7@gmail.com>
2022-11-12 08:04:23 +0900

    [Feat] CreateTodoVC 할 일 컴포넌트들 추가 및 레이아웃 구성

--- contents/TODO_List/TODO_List/CreateTodoVC.swift
+++ contents/TODO_List/TODO_List/CreateTodoVC.swift
@@ -10,6 +10,7 @@ final class CreateTodoViewController: UIViewController {
   private let stateLabel = UIFactory.label("상태", textColor: .black)
   private let closeButton = UIFactory.plainButton("닫기", textColor: .black)
   private let saveButton = UIFactory.plainButton("저장", textColor: .black)
+  private let textViewLine = UIFactory.separatorLine()
   
   private var titleTextView: UITextView!
   
@@ -19,13 +20,25 @@ final class CreateTodoViewController: UIViewController {
     super.viewDidLoad()
     setupTextView()
     setupStyles()
+    setupLayout()
+    setupConstraints()
+    setupButton()
   }
   
   // MARK: - Setup
+  
+  private func setupButton() {
+    closeButton.addTarget(self, action: #selector(didTapCloseButton), for: .touchUpInside)
+    saveButton.addTarget(self, action: #selector(didTapSaveButton), for: .touchUpInside)
+    saveButton.isEnabled = false
+  }
 
   private func setupTextView() {
     let textView = UITextView()
     textView.isScrollEnabled = false
+    textView.backgroundColor = .white
+    textView.textColor = .black
+    textView.font = .systemFont(ofSize: 18, weight: .regular)
     self.titleTextView = textView
   }
   
@@ -33,14 +46,28 @@ final class CreateTodoViewController: UIViewController {
     view.backgroundColor = .white
     navigationItem.leftBarButtonItem = UIBarButtonItem(customView: closeButton)
     navigationItem.rightBarButtonItem = UIBarButtonItem(customView: saveButton)
-    saveButton.isEnabled = false
   }
   
   private func setupLayout() {
+    [todoLabel, titleTextView, textViewLine]
+      .forEach { view.addSubview($0) }
   }
   
   private func setupConstraints() {
+    todoLabel.snp.makeConstraints { make in
+      make.leading.top.equalTo(view.safeAreaLayoutGuide).inset(24)
+    }
     
+    titleTextView.snp.makeConstraints { make in
+      make.leading.trailing.equalToSuperview().inset(24)
+      make.top.equalTo(todoLabel.snp.bottom).offset(8)
+    }
+    
+    textViewLine.snp.makeConstraints { make in
+      make.leading.trailing.equalToSuperview().inset(24)
+      make.top.equalTo(titleTextView.snp.bottom)
+      make.height.equalTo(1)
+    }
   }
   
   // MARK: - Selectors
