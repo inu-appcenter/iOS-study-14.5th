b22027ce1e12771b5e53d7d9088896ee7981f550
이창준 <crystalashe@naver.com>
2022-11-11 16:13:35 +0900

    [Feat] 날짜 선택 컬렉션뷰 파일 생성

---
+++ contents/ToDo/changjun/todoey/todoey/Presentation/Home/View/DateSelectorCollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  DateSelectorCollectionView.swift
+//  todoey
+//
+//  Created by 이창준 on 2022/11/11.
+//
+
+import UIKit
+
+class DateSelectorCollectionView: UICollectionView {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ contents/ToDo/changjun/todoey/todoey/Presentation/Home/View/DateSelectorCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  DateSelectorCollectionViewCell.swift
+//  todoey
+//
+//  Created by 이창준 on 2022/11/11.
+//
+
+import UIKit
+
+class DateSelectorCollectionViewCell: UICollectionViewCell {
+    
+}
