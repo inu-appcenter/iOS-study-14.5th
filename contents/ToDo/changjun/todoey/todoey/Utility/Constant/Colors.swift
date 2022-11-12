8742730d394af704e28df6ffb9232f779d8db8ad
이창준 <crystalashe@naver.com>
2022-11-12 13:24:13 +0900

    [Feat] Color 파일 추가 (Hue 라이브러리 사용)

---
+++ contents/ToDo/changjun/todoey/todoey/Utility/Constant/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  todoey
+//
+//  Created by 이창준 on 2022/11/12.
+//
+
+import Foundation
+
+import Hue
+
+struct Color {
+    let deepGray = UIColor(hex: "#2D2D2D")
+    let lightGray = UIColor(hex: "#F5F5F5")
+}
