5d3339936c61cc7903bfd06590b1725d0cf8dcb1
eung7 <wannabeeung7@gmail.com>
2022-11-12 11:10:28 +0900

    [Feat] TodoCell 구현

--- contents/TODO_List/TODO_List/TodoCell.swift
+++ contents/TODO_List/TODO_List/TodoCell.swift
@@ -1,3 +1,42 @@
 import UIKit
 
 import SnapKit
+import Then
+
+final class TodoCell: UITableViewCell {
+  
+  // MARK: - Properties
+  
+  static let identifier = "TodoCell"
+  
+  private let titleLabel = UIFactory.label("", textColor: .black).then {
+    $0.numberOfLines = 0
+  }
+    
+  // MARK: - Setup
+  
+  func setupComponents(_ todo: Todo) {
+    setupLayout()
+    setupConstraints()
+    titleLabel.text = todo.title
+    switch todo.state {
+    case .progress:
+      titleLabel.textColor = .black
+    case .cancel:
+      titleLabel.textColor = .lightGray
+      titleLabel.attributedText = titleLabel.text?.strikeThrough()
+    case .completed:
+      titleLabel.textColor = .lightGray
+    }
+  }
+  
+  private func setupLayout() {
+    contentView.addSubview(titleLabel)
+  }
+  
+  private func setupConstraints() {
+    titleLabel.snp.makeConstraints { make in
+      make.edges.equalToSuperview().inset(16)
+    }
+  }
+}
