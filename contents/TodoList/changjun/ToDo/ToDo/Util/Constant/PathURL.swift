13ce2bba5c69e647cc7359d7d7539becf4fcb9c3
이창준 <crystalashe@naver.com>
2022-12-01 21:32:16 +0900

    [Chore] BaseURL 숨김 처리

--- contents/TodoList/changjun/ToDo/ToDo/Util/Constant/PathURL.swift
+++ contents/TodoList/changjun/ToDo/ToDo/Util/Constant/PathURL.swift
@@ -1,15 +1,15 @@
 //
-//  Network.swift
+//  PathURL.swift
 //  ToDo
 //
-//  Created by 이창준 on 2022/11/29.
+//  Created by 이창준 on 2022/11/26.
 //
+
+import Foundation
 
 /// swagger docs 주소
 /// https://eg-todo.inuappcenter.kr/swagger-ui.html#/
 ///
-
-import Foundation
 
 enum PathURL {
     /// BaseURL/{id}
@@ -17,13 +17,6 @@ enum PathURL {
     /// - DELETE: 회원 탈퇴
     /// - PATCH: 회원 정보 수정
     static let members = "/members/"
-    static let checkToken = "/members"
     static let signup = "/members/sign-up" // body required
     static let login = "/members/sign-in" // body required
-    static let todos = "/todos"
-    static let todosRequest = "/todos/" // + {todoId}
-}
-
-enum Header {
-    static let authToken = "X-AUTH-TOKEN"
 }
--- contents/TodoList/changjun/ToDo/ToDo/Util/Constant/UserDefaultsKey.swift
+++ contents/TodoList/changjun/ToDo/ToDo/Util/Constant/UserDefaultsKey.swift
@@ -8,17 +8,9 @@
 import Foundation
 
 enum UserDefaultsKey {
-    /// todo: ToDo
     static let todo = "todo"
-    /// login
     static let login = "login"
-    /// authToken: String
     static let authToken = "authToken"
-    /// lastValidAuth: Auth
-    static let lastValidAuth = "lastValidAuth"
     
-    /// authResponse: AuthResponse
-    static let authResponse = "authResponse"
-    /// userName: String
     static let userName = "userName"
 }
